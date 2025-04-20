f92f5e9f3e127a6959ac8681bafd3f7da29ae682
kazuhiro <moriyama.kazuhiro@earthsys-lab.co.jp>
2025-04-20 20:38:58 +0900

    プロジェクトのセットアップ

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+
+app.get('/',(req, res) => {
+  res.json({message: 'Hello World'})
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
