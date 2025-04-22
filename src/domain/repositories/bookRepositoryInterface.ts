7301a09c5ee820b9e29483a858a51672aabc010a
kazuhiro <moriyama.kazuhiro@earthsys-lab.co.jp>
2025-04-23 01:35:12 +0900

    エンティティ層の実装

--- src/domain/repositories/bookRepositoryInterface.ts
+++ src/domain/repositories/bookRepositoryInterface.ts
@@ -2,5 +2,5 @@ import { Book } from "../entities/book";
 
 export interface BookRepositoryInterface {
   create(book: Book): Promise<Book>;
-  findById(id: string): Promise<Book | null>;
+  // findById(id: string): Promise<Book | null>;
 }
