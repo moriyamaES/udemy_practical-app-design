18b04b882868d0f7e27c90a6b86740c84857193f
kazuhiro <moriyama.kazuhiro@earthsys-lab.co.jp>
2025-04-23 23:04:18 +0900

    インターフェースアダプター層の実装（Controller）

--- src/adpter/controllers/bookController.ts
+++ src/adpter/controllers/bookController.ts
@@ -1,13 +1,17 @@
 import { Request, Response } from "express";
-import { BookServiceInterface } from "../businessLogic/bookServiceInterface";
+import { AddBookUseCaseInterface } from "../../application/useCases/book/addBookUseCaseInterface";
+import { AddBookRequestDto } from "../../application/dtos/book/addBookRequestDto";
 
 export class BookController {
-  constructor(private readonly bookService: BookServiceInterface) {}
+  constructor(private readonly addBookUseCase: AddBookUseCaseInterface) {}
   
   async add(req: Request, res: Response): Promise<void> {
     try {
+      const requestDto: AddBookRequestDto = {
+        title: req.body.title,
+      }
       const title = req.body.title as string;
-      const book = await this.bookService.add(title);
+      const book = await this.addBookUseCase.execute(requestDto);
       res.status(201).json(book);
     } catch (error) {
       console.log(error);
@@ -15,18 +19,18 @@ export class BookController {
     }
   }
 
-  async findById(req: Request, res: Response): Promise<void> {
-    try {
-      const id = req.params.id as string;
-      const book = await this.bookService.findById(id);
-      if (book) {
-        res.status(200).json(book);
-      } else {
-        res.status(404).json({ error: '書籍が見つかりません' });
-      }
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: '書籍の取得に失敗しました' });
-    }
-  }
+  // async findById(req: Request, res: Response): Promise<void> {
+  //   try {
+  //     const id = req.params.id as string;
+  //     const book = await this.bookService.findById(id);
+  //     if (book) {
+  //       res.status(200).json(book);
+  //     } else {
+  //       res.status(404).json({ error: '書籍が見つかりません' });
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //     res.status(500).json({ error: '書籍の取得に失敗しました' });
+  //   }
+  // }
 }
