dee4122e1eed348228df1890055a7951b6a14daf
kazuhiro <moriyama.kazuhiro@earthsys-lab.co.jp>
2025-04-23 21:17:44 +0900

    インターフェースアダプター層の実装（Repoditories）

--- src/adpter/repoditories/prismaBookRepository.ts
+++ src/adpter/repoditories/prismaBookRepository.ts
@@ -1,26 +1,31 @@
 import { PrismaClient } from "@prisma/client";
-import { Book } from "@prisma/client";
-import { BookRepositoryInterface } from "./bookRepositoryInterface";
+import { Book } from "../../domain/entities/book";
+import { BookRepositoryInterface } from "../../domain/repositories/bookRepositoryInterface";
 
 export class PrismaBookRepository implements BookRepositoryInterface {
-  private prisma: PrismaClient;
+  constructor(private readonly prisma: PrismaClient) {}
 
-  constructor() {
-    this.prisma = new PrismaClient();
-  }
-
-  async create(title: string): Promise<Book> {
-    return await this.prisma.book.create({
+  async create(book: Book): Promise<Book> {
+    const createdBook = await this.prisma.book.create({
       data: {
-        title,
-        isAvailable: true,
+        id: book.id,
+        title: book.title,
+        isAvailable: book.isAvailable,
+        createdAt: book.createdAt,
+        updatedAt: book.updatedAt,
       },
     });
+    return new Book(
+      createdBook.id,
+      createdBook.title,
+      createdBook.isAvailable,
+      createdBook.createdAt,
+      createdBook.updatedAt
+    );
   }
-
-  async findById(id: string): Promise<Book | null> {
-    return await this.prisma.book.findUnique({
-      where: { id },
-    });
-  }
+  // async findById(id: string): Promise<Book | null> {
+  //   return await this.prisma.book.findUnique({
+  //     where: { id },
+  //   });
+  // }
 }
