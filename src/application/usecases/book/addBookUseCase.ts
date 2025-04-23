0142876fef68bba24ef38f5a4e52cd86d9597849
kazuhiro <moriyama.kazuhiro@earthsys-lab.co.jp>
2025-04-23 19:27:59 +0900

    ユースケース層の実装

--- src/application/usecases/book/addBookUseCase.ts
+++ src/application/usecases/book/addBookUseCase.ts
@@ -1,11 +1,26 @@
 import { AddBookUseCaseInterface } from "./addBookUseCaseInterface";
 import { BookRepositoryInterface } from "../../../domain/repositories/bookRepositoryInterface";
-import { privateDecrypt } from "crypto";
 import { IdGeneratorInterface } from "../../../domain/utils/idGeneratorInterface";
+import { AddBookRequestDto } from "../../dtos/book/addBookRequestDto";
+import { AddBookResponseDto } from "../../dtos/book/addBookResponseDto";
+import { Book } from "../../../domain/entities/book";
 
 export class AddBookUseCase implements AddBookUseCaseInterface {
   constructor(
     private readonly bookRepository: BookRepositoryInterface,
     private readonly idGenerator: IdGeneratorInterface
   ) {}
+  async execute(requestDto: AddBookRequestDto): Promise<AddBookResponseDto> {
+    const id = this.idGenerator.generate();
+    const newBook = new Book(id, requestDto.title)
+    const createdBook = await this.bookRepository.create(newBook);
+    return {
+      id: createdBook.id,
+      title: createdBook.title,
+      isAvailable: createdBook.isAvailable,
+      createdAt: createdBook.createdAt,
+      updatedAt: createdBook.updatedAt,
+    };
+  };
+    
 }
