0d347d3af5d7693f0a3b3dcb6ca4cc0e20a609ab
kazuhiro <moriyama.kazuhiro@earthsys-lab.co.jp>
2025-04-26 19:46:45 +0900

    ツールのメインファイル作成

---
+++ tools/generateFiles.ts
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+import { listenerCount } from "process";
+
+async function main() {
+  const layers = [
+    'Entity',
+    'UseCase',
+    'Interface adapter',
+    'Framework & driver',
+  ] as const
+
+  type Layer = (typeof layers)[number]
+
+  const { layer }: {layer: Layer} = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'layer',
+      message: 'どの層のファイルを生成しますか？',
+      choices: layers,
+    }
+  ])
+
+  if (layer === 'Entity'){
+    console.log('Entity')
+  } else if (layer === 'UseCase') {
+    console.log('UseCase')
+  } else if (layer === 'Interface adapter') {
+    console.log('Interface adapter')
+  } else if (layer === 'Framework & driver') {
+    console.log('Framework & driver')
+  }
+}
+
+main()
