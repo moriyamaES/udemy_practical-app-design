3f3e6a51374c823983ed6c926e8c7ed958342ad1
kazuhiro <moriyama.kazuhiro@earthsys-lab.co.jp>
2025-04-26 17:02:32 +0900

    インターフェースアダプター層の実装（UUID）

---
+++ src/adpter/utils/uuidGenerator.ts
@@ -0,0 +1,9 @@
+import { v4 as uuidv4 } from 'uuid';
+import { IdGeneratorInterface } from '../../domain/utils/idGeneratorInterface';
+
+export class UuidGenerator implements IdGeneratorInterface {
+  generate(): string {
+    return uuidv4();
+  }
+}
+
