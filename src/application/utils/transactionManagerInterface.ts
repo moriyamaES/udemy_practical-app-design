308c5dd61cda08abc5420ef8f664c66a817130a3
kazuhiro <moriyama.kazuhiro@earthsys-lab.co.jp>
2025-04-29 21:08:08 +0900

    トランザクション管理の実装Part1

---
+++ src/application/utils/transactionManagerInterface.ts
@@ -0,0 +1,8 @@
+import { TransactionContextInterface } from "../../domain/utils/transactionContextInterface";
+
+export interface TransactionManagerInterface {
+  run<T>(
+    operation: (ctx: TransactionContextInterface) => Promise<T>
+  ): Promise<T>
+}
+
